7562531bfe87b891ffc66781ca2bfb0a9b8d9e62
Alexander Lee Smith <63920477+lapsangsouchy@users.noreply.github.com>
2024-07-28 19:13:06 -0400

    Main Solutions Uploaded

---
+++ while_loops_project.js
@@ -0,0 +1,83 @@
+/* Part 1a - Counting Down */
+let counter = 100;
+
+while (counter >= 0) {
+  console.log(counter);
+  counter--;
+}
+
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+/* Part 1b - Counting Down + User Input */
+let userCounter = Number(process.argv[2]);
+
+while (userCounter >= 0) {
+  console.log(userCounter);
+  userCounter--;
+}
+
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+/* Part 2 - While Loop Gymnastics */
+
+/* Question #1: */
+// Three Times
+
+/* Question #2: */
+// Infinitely!
+
+/* Question #3: */
+// Five Times
+
+/* Question #4: */
+// Two Times
+
+/* Question #5: */
+let num = 0;
+
+// Change from > to <
+while (num < 10) {
+  // Remove the break from this line
+  console.log('Little Donald Drumpf');
+  num++;
+}
+
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+/* Part 3a - Two Random Numbers */
+let x = 0;
+while (x < 1) {
+  let randNum1 = Math.floor(Math.random() * 11);
+  let randNum2 = Math.floor(Math.random() * 11);
+
+  console.log('1st random number:', randNum1, '2nd random number:', randNum2);
+  if (randNum1 == randNum2) {
+    console.log('Same random numbers! End Loop');
+    break;
+  }
+}
+
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+/* Part 3b - Two Random Numbers */
+// Note: You can also create the infinite loop with the tryCount by setting
+//       while (tryCount > 0) or something that will always be true!
+let tryCount = 1;
+
+while (x < 1) {
+  let randNum1 = Math.floor(Math.random() * 11);
+  let randNum2 = Math.floor(Math.random() * 11);
+  console.log(
+    'Try #' + tryCount,
+    '-',
+    '1st random number:',
+    randNum1,
+    '2nd random number:',
+    randNum2
+  );
+  if (randNum1 == randNum2) {
+    console.log('Same random numbers on try #' + tryCount + '! End Loop');
+    break;
+  }
+  tryCount++;
+}
+
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+/* Extra Credit - Binary Numbers */
+// To Be Completed Later
